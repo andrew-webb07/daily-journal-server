9fb90d5237d52d6a8a92a63fdcb97ce8788b4a71
Andrew Webb <webba@ihopu.org>
2021-07-22 13:43:59 -0500

    added update function to entries

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from entries.request import create_entry
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from entries import get_single_entry, get_all_entries, delete_entry, search_entries, create_entry
+from entries import get_single_entry, get_all_entries, delete_entry, search_entries, create_entry, update_entry
 import json
 
 
@@ -80,6 +80,23 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == "entries":
             new_entry = create_entry(post_body)
             self.wfile.write(f"{new_entry}".encode())
+
+    def do_PUT(self):
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+        (resource, id) = self.parse_url(self.path)
+        success = False
+
+        if resource == "entries":
+            success = update_entry(id, post_body)
+        
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
+
+        self.wfile.write("".encode())
 
 def main():
     host = ''
